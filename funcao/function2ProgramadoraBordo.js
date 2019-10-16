9cbd3f7ad48720027f0e0dc336a63602ae7e47fb
Tiago <tiagoestevam7@hotmail.com>
2019-10-16 17:08:30 -0300

    revisaoFunction

---
+++ funcao/function2ProgramadoraBordo.js
@@ -0,0 +1,21 @@
+function Usuario() {
+  this.nome = 'Tel'
+  this.idade = 27;
+
+}
+let usuario = new Usuario();
+Usuario();
+
+console.log(usuario);
+
+function Personagem(p1, p2, p3) {
+  console.log(p1);
+  console.log(p2);
+  console.log(p3);
+  console.log('PersonagemThis', this)
+}
+let perso = {
+  nome: 'Ernandes'
+}
+
+Personagem.call(perso, 'parametro1', 855, [8, 6, 9]);
